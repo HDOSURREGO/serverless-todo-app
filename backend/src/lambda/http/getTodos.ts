b6687760924437165566a5008db0d2365457cf4b
Henry Hoyos <hdosurrego@gmail.com>
2020-06-01 17:29:04 -0400

    GetTodos Lambda F(x) included

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,13 +1,45 @@
-import 'source-map-support/register'
-
 import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  APIGatewayProxyHandler
-} from 'aws-lambda'
+	APIGatewayProxyEvent,
+	APIGatewayProxyResult,
+	APIGatewayProxyHandler,
+} from "aws-lambda";
+import "source-map-support/register";
+import * as AWS from "aws-sdk";
+import { createLogger } from "../../utils/logger";
+import { getUserId } from "../utils";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todoTable = process.env.TODO_ITEMS;
+const logger = createLogger("getTodosFunctionLog");
 
 export const handler: APIGatewayProxyHandler = async (
-  event: APIGatewayProxyEvent
+	event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-}
+	// TODO: Get all TODO items for a current user
+	const userId = getUserId(event);
+
+	logger.info("Processing event: ", event);
+
+	const result = await docClient
+		.query({
+			TableName: todoTable,
+			KeyConditionExpression: "userId = :userId",
+			ExpressionAttributeValues: {
+				":userId": userId,
+			},
+			ScanIndexForward: false,
+		})
+		.promise();
+
+	const items = result.Items;
+
+	return {
+		statusCode: 200,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+		},
+		body: JSON.stringify({
+			items,
+		}),
+	};
+};
