bb59b846fdea7d44656e3ee829b65ca2fcb3de61
Henry Hoyos <hdosurrego@gmail.com>
2020-06-04 23:49:14 -0400

    updateTodo.ts added

---
+++ backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,57 @@
+import "source-map-support/register";
+import * as AWS from "aws-sdk";
+import {
+	APIGatewayProxyEvent,
+	APIGatewayProxyHandler,
+	APIGatewayProxyResult,
+} from "aws-lambda";
+import { createLogger } from "../../utils/logger";
+import { UpdateTodoRequest } from "../../requests/UpdateTodoRequest";
+
+const logger = createLogger("Update-Todo-Log");
+
+export const handler: APIGatewayProxyHandler = async (
+	event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+	// const todoId = event.pathParameters.todoId;
+
+	logger.info("Processing event: ", event);
+
+	const todoId = "345";
+	const userId = "123";
+	const updatedTodo: UpdateTodoRequest = JSON.parse(event.body);
+	const docClient = new AWS.DynamoDB.DocumentClient();
+	const todosTable = process.env.TODO_ITEMS;
+
+	// TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+
+	await docClient
+		.update({
+			TableName: todosTable,
+			Key: {
+				userId: userId,
+				todoId: todoId,
+			},
+			ConditionExpression: "todoId = :todoId",
+			UpdateExpression: "set #n = :name, dueDate = :dueDate, done = :done",
+			ExpressionAttributeValues: {
+				":name": updatedTodo.name,
+				":dueDate": updatedTodo.dueDate,
+				":done": updatedTodo.done,
+				":todoId": todoId,
+			},
+			ExpressionAttributeNames: {
+				"#n": "name",
+			},
+			ReturnValues: "UPDATED_NEW",
+		})
+		.promise();
+
+	return {
+		statusCode: 200,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+		},
+		body: "Update Successfull",
+	};
+};
