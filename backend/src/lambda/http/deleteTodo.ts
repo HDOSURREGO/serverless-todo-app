612c3ec72f09b11b4b377d9a35193e0649900db7
Henry Hoyos <hdosurrego@gmail.com>
2020-06-06 22:32:43 -0400

    deleteTodo working

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,16 +1,47 @@
 import "source-map-support/register";
-
+import { createLogger } from "../../utils/logger";
 import {
 	APIGatewayProxyEvent,
 	APIGatewayProxyResult,
 	APIGatewayProxyHandler,
 } from "aws-lambda";
+import * as AWS from "aws-sdk";
+
+const logger = createLogger("Todo-Access-Layer");
 
 export const handler: APIGatewayProxyHandler = async (
 	event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-	const todoId = event.pathParameters.todoId;
-
 	// TODO: Remove a TODO item by id
-	return undefined;
+
+	console.log("Delete in Process", event);
+
+	const userId = "123";
+	const todoId = "345";
+	const todosTable = process.env.TODO_ITEMS;
+	const docClient = new AWS.DynamoDB.DocumentClient();
+
+	logger.info("Delete todo " + todoId + "for user" + userId);
+
+	await docClient
+		.delete({
+			TableName: todosTable,
+			Key: {
+				userId: userId,
+				todoId: todoId,
+			},
+			ConditionExpression: "todoId = :todoId",
+			ExpressionAttributeValues: {
+				":todoId": todoId,
+			},
+		})
+		.promise();
+
+	return {
+		statusCode: 204,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+		},
+		body: "Item Deleted",
+	};
 };
