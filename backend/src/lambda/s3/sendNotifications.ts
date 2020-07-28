abe4a83d9cb761db34d24569012e4a7c1f16d01d
Henry Hoyos <hdosurrego@gmail.com>
2020-07-27 20:35:23 -0400

    back to Jimp

--- backend/src/lambda/s3/sendNotifications.ts
+++ backend/src/lambda/s3/sendNotifications.ts
@@ -1,9 +1,9 @@
 import { SNSHandler, SNSEvent } from "aws-lambda";
 import { createLogger } from "../../utils/logger";
-// import Jimp from "jimp";
+import Jimp from "jimp";
 // import fs from "fs";
 import * as AWS from "aws-sdk";
-import sharp from "sharp";
+// import sharp from "sharp";
 
 const s3 = new AWS.S3();
 const logger = createLogger("S3-Event-File_Uploaded");
@@ -26,12 +26,49 @@ export const handler: SNSHandler = async (event: SNSEvent) => {
 			const key = record.s3.object.key;
 			logger.info(key);
 			if (!key.includes("_bw.jpg")) {
-				// const linkUrl: string =
-				// "https://" + bucketName + ".s3.amazonaws.com/" + key;
+				const linkUrl: string =
+					"https://" + bucketName + ".s3.amazonaws.com/" + key;
 				try {
 					// const resultado = await filterImageFromURL(linkUrl);
-					const resultado = await createThumbnail(key);
-					console.log(resultado);
+
+					// const fileBuffer = fs.readFileSync(resultado);
+
+					Jimp.read(linkUrl)
+						.then(async (image) => {
+							const buffer = await image
+								.resize(256, 256) // resize
+								.quality(60) // set JPEG quality
+								.greyscale() // set greyscale
+								.getBufferAsync(Jimp.MIME_JPEG);
+							const destparams = {
+								Bucket: bucketName,
+								Key: key + "_bw.jpg",
+								Body: buffer,
+								ContentType: "image",
+							};
+							await s3.putObject(destparams).promise();
+						})
+						.catch((e) => {
+							logger.error(e);
+							console.log("Something went terribly wrong!");
+						});
+
+					// Upload the thumbnail image to the destination bucket
+					// try {
+					// 	const destparams = {
+					// 		Bucket: bucketName,
+					// 		Key: key + "_bw.jpg",
+					// 		Body: fileBuffer,
+					// 		ContentType: "image",
+					// 	};
+					// 	await s3.putObject(destparams).promise();
+					// } catch (error) {
+					// 	console.log(error);
+					// 	return;
+					// }
+
+					// // const resultado = await createThumbnail(key);
+					// console.log(resultado);
 				} catch (error) {
 					console.log("Este es mi error: ", error);
 				}
@@ -40,51 +77,12 @@ export const handler: SNSHandler = async (event: SNSEvent) => {
 	}
 };
 
-async function createThumbnail(key: string) {
-	// Download the image from the S3 source bucket.
-	let origimage;
-	try {
-		const params = {
-			Bucket: bucketName,
-			Key: key,
-		};
-		origimage = await s3.getObject(params).promise();
-	} catch (error) {
-		console.log(error);
-		return;
-	}
-
-	// set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
-	const width = 200;
-	// Use the Sharp module to resize the image and save in a buffer.
-	let buffer;
-	try {
-		buffer = await sharp(origimage.Body).resize(width).toBuffer();
-	} catch (error) {
-		console.log(error);
-		return;
-	}
-
-	// Upload the thumbnail image to the destination bucket
-	try {
-		const destparams = {
-			Bucket: bucketName,
-			Key: key + "_bw.jpg",
-			Body: buffer,
-			ContentType: "image",
-		};
-		await s3.putObject(destparams).promise();
-	} catch (error) {
-		console.log(error);
-		return;
-	}
-}
-
 // async function filterImageFromURL(inputURL: string): Promise<string> {
 // 	return new Promise(async (resolve) => {
 // 		const photo = await Jimp.read(inputURL);
 // 		const outpath =
 // 			"/tmp/filtered." + Math.floor(Math.random() * 2000) + "_bw.jpg";
+// 		const buffer = await photo.quality(1).getBufferAsync(jimp.MIME_JPEG);
 // 		await photo
 // 			.resize(256, 256) // resize
 // 			.quality(60) // set JPEG quality
@@ -95,6 +93,45 @@ async function createThumbnail(key: string) {
 // 	});
 // }
 
+// async function createThumbnail(key: string) {
+// 	// Download the image from the S3 source bucket.
+// 	let origimage;
+// 	try {
+// 		const params = {
+// 			Bucket: bucketName,
+// 			Key: key,
+// 		};
+// 		origimage = await s3.getObject(params).promise();
+// 	} catch (error) {
+// 		console.log(error);
+// 		return;
+// 	}
+
+// set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
+// const width = 200;
+// Use the Sharp module to resize the image and save in a buffer.
+// 	let buffer;
+// 	try {
+// 		buffer = await sharp(origimage.Body).resize(width).toBuffer();
+// 	} catch (error) {
+// 		console.log(error);
+// 		return;
+// 	}
+
+// 	// Upload the thumbnail image to the destination bucket
+// 	try {
+// 		const destparams = {
+// 			Bucket: bucketName,
+// 			Key: key + "_bw.jpg",
+// 			Body: buffer,
+// 			ContentType: "image",
+// 		};
+// 		await s3.putObject(destparams).promise();
+// 	} catch (error) {
+// 		console.log(error);
+// 		return;
+// 	}
+// }
 // deleteLocalFiles
 // helper function to delete files on the local disk
 // useful to cleanup after tasks
