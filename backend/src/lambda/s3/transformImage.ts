358366734f24fb0f99f8f1864c0130ded8d95f8f
Henry Hoyos <hdosurrego@gmail.com>
2020-07-28 08:09:33 -0400

    transformImage

--- backend/src/lambda/s3/transformImage.ts
+++ backend/src/lambda/s3/transformImage.ts
@@ -20,23 +20,15 @@ export const handler: SNSHandler = async (event: SNSEvent) => {
 		const s3EventStr = snsRecord.Sns.Message;
 		const s3Event = JSON.parse(s3EventStr);
 
-		console.log("Este es el evento que llegó ", s3Event);
-
 		for (const record of s3Event.Records) {
 			const key = record.s3.object.key;
-			logger.info("Este el nombre del archivo subido al S3: ", key);
-			console.log("Este el nombre del archivo subido al S3: ", key);
+			logger.info("This is the file to upload to S3: ", key);
+			console.log("This is the file to upload to S3: ", key);
 			if (!key.includes("_bw.jpg")) {
 				const linkUrl: string =
 					"https://" + bucketName + ".s3.amazonaws.com/" + key;
-				logger.info("Este es el link para transformar: ", linkUrl);
-				console.log("Este es el link para transformar: ", linkUrl);
-				try {
-					// const resultado = await filterImageFromURL(linkUrl);
 
-					// const fileBuffer = fs.readFileSync(resultado);
-					logger.info("Entre al try donde se hace la transformacion: ");
-					console.log("Entre al try donde se hace la transformacion: ");
+				try {
 					await Jimp.read(linkUrl)
 						.then(async (image) => {
 							const buffer = await image
@@ -50,30 +42,14 @@ export const handler: SNSHandler = async (event: SNSEvent) => {
 								Body: buffer,
 								ContentType: "image",
 							};
-							logger.info(
-								"Estos son los parametros despues de transformar: ",
-								destparams
-							);
-							console.log(
-								"Estos son los parametros despues de transformar: ",
-								destparams
-							);
 							await s3.putObject(destparams).promise();
-							logger.info(
-								"Y aqui ya debe estar en el S3 guardado: ",
-								destparams
-							);
-							console.log(
-								"Y aqui ya debe estar en el S3 guardado: ",
-								destparams
-							);
 						})
 						.catch((e) => {
 							logger.error(e);
-							console.log("Jueputa!! Something went terribly wrong!");
+							console.log("Something went terribly wrong!");
 						});
 				} catch (error) {
-					console.log("Este es mi error: ", error);
+					console.log("This is my error: ", error);
 				}
 			}
 		}
